9eee879589fb51423cb8e8d557d4f536725bea98
Flugschwein <flugschwein@gmx.at>
2018-10-07 15:30:29 +0200

    Add more feature to reward balance graph

--- hf20-drop/reward_balance.py
+++ hf20-drop/reward_balance.py
@@ -10,23 +10,25 @@ for e in funds:
 
 funds = sorted(funds, key=lambda x: x['last_update'])
 now = datetime.datetime.now()
-start = now - datetime.timedelta(days=7)
-date = []
-balance = []
-recent_claims = []
-for e in funds:
-    if e['last_update'] < start:
-        continue
-    date.append(e['last_update'])
-    balance.append(e['reward_balance'])
-    recent_claims.append(e['recent_claims'])
-plt.plot(date, balance, color='r')
-plt.title('Recent Claims and Reward Balance - Last 450 days')
-plt.xlabel('Date')
-plt.ylabel('Reward Balance', color='r')
-plt.yticks(color='r')
-plt.twinx()
-plt.plot(date, recent_claims, color='b')
-plt.ylabel('Recent Claims', color='b')
-plt.yticks(color='b')
+for cnt, i in enumerate([7, 14]):
+    start = now - datetime.timedelta(days=7)
+    date = []
+    balance = []
+    recent_claims = []
+    for e in funds:
+        if e['last_update'] < start:
+            continue
+        date.append(e['last_update'])
+        balance.append(e['reward_balance'])
+        recent_claims.append(e['recent_claims'])
+    plt.subplot(2, 1, cnt+1)
+    plt.plot(date, balance, color='r')
+    plt.title('Recent Claims and Reward Balance - Last 450 days')
+    plt.xlabel('Date')
+    plt.ylabel('Reward Balance', color='r')
+    plt.yticks(color='r')
+    plt.twinx()
+    plt.plot(date, recent_claims, color='b')
+    plt.ylabel('Recent Claims', color='b')
+    plt.yticks(color='b')
 plt.show()
